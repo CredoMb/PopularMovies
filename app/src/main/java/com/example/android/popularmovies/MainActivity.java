ee0909595ed3b40392f8b9b711f25e01fa8d0f2d
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-03-01 22:47:34 -0300

    Add the Loader to manage background threading

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -7,10 +7,18 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration;
 
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.android.popularmovies.Data.MovieLoader;
 import com.example.android.popularmovies.Data.QueryUtils;
 import com.example.android.popularmovies.Fragment.GridSpacingItemDecoration;
 
@@ -20,7 +28,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<AMovie>>{
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
@@ -67,10 +75,79 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mMovietAdapter);
 
         /** Start the Loader */
+
+    }
+
+    /**
+     * Method to Check the Network connection and return true or false
+     * based on the network connection state
+     */
+    private boolean isNetworkConnected() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
     }
 
     private int dpToPx(int dp) {
         float px = dp* this.getResources().getDisplayMetrics().density;
         return (int) px;
     }
+    /** Get executed when the loader is initiated */
+    @Override
+    public Loader<List<AMovie>> onCreateLoader(int i, Bundle bundle) {
+
+        // Make an Uri Builder with the Google Request Url as the base Uri
+        Uri baseUri = Uri.parse(MOVIE_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Add 2 query parameter to the base Uri.
+        // maxResults : determine the maximum number of elements
+        // that should be downloaded from the API.
+        // q: represent the book genre for the books that will be
+        // downloaded from the API.
+
+        uriBuilder.appendQueryParameter("maxResults", MAX_RESULT);
+        uriBuilder.appendQueryParameter("q", mBookGenre.trim());
+
+        // This will execute the network request needed to get the data
+        // from the API and return the data to onLoadFinished
+        return new MovieLoader(this, uriBuilder.toString());
+    }
+
+    /** Get executed when the background thread finishes the work */
+    @Override
+    public void onLoadFinished(Loader<List<AMovie>> loader, List<AMovie> data) {
+
+        // Set empty state view with the content of the "no result" state
+        // This will explain to the user that no result where found for the
+        // key word he entered
+        /*fillEmptyStateView(R.drawable.empty_state_search,
+                R.string.no_results_title,
+                R.string.no_results_subtitle);*/
+
+        // Hide the loading spinner
+        /*mProgressSpinner.setVisibility(View.GONE);*/
+
+        // Clear the adapter of the data of previous books
+        mAdapter.clear();
+
+        // If there is a valid list of {@link Book}s, then add them to the adapter's
+        // data set. This will trigger the ListView to update.
+
+        if (data != null && !data.isEmpty()) {
+            mAdapter.addAll(data);
+        }
+    }
+
+    /** This will reset the previous created loader */
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+
+        // Create a new empty book list for the Adapter
+        mAdapter.addAll(new ArrayList<Book>());
+    }
+
 }
