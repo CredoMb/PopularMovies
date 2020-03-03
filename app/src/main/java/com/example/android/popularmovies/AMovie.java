fa51b9ee68acd457233603d39e8805871b1ae1f2
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-03-02 22:14:57 -0300

    Add  more attributes in the AMovie classm

--- app/src/main/java/com/example/android/popularmovies/AMovie.java
+++ app/src/main/java/com/example/android/popularmovies/AMovie.java
@@ -1,5 +1,8 @@
 package com.example.android.popularmovies;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AMovie {
 
     // The id of the movie's image
@@ -10,10 +13,21 @@ public class AMovie {
 
     // The movie's year
     private String mYear;
+
+    // The synopsis of the movie
+    private String mSynopsis;
+    // The director of the movie
+    private String mMovieDirector;
+    // A list that contains all the movie stars
+    private List<String> mMovieStars;
+    // The average rating of the movie
+    private float mMovieRating;
+
+
     /**
-     * Constructor
+     * Constructor with the 3 main attribute of the class
      */
-    public AMovie (String title,String imageUrl,String year) {
+    public AMovie(String title, String imageUrl, String year) {
         mTitle = title;
         mImageUrl = imageUrl;
         mYear = year;
@@ -26,9 +40,51 @@ public class AMovie {
     public String getImageId() {
         return mImageUrl;
     }
+
     public String getTitle() {
         return mTitle;
     }
-    public String getYear() {return mYear;}
+
+    public String getSynopsis() {
+        return mSynopsis;
+    }
+
+    public String getMovieDirector() {
+        return mMovieDirector;
+    }
+
+    public List<String> getMovieStars() {
+        return mMovieStars;
+    }
+
+    public float getMovieRating() {
+        return mMovieRating;
+    }
+
+    /**
+     * Defining setters for certain attributes of the "AMovie" class.
+     * We will define setters only for a certain attributes.
+     */
+    public void setSynopsis(String synopsis) {
+        mSynopsis = synopsis;
+    }
+
+    public void setMovieStars(List<String> movieStars) {
+
+        // Initialise the list before putting values in
+        mMovieStars = new ArrayList<String>();
+
+        for (int i = 0; i < movieStars.size(); i++) {
+            mMovieStars.set(i, movieStars.get(i));
+        }
+    }
+
+    public void setMovieDirector(String movieDirector) {
+        mMovieDirector = movieDirector;
+    }
+
+    public void setMovieRating(float movieRating) {
+        mMovieRating = movieRating;
+    }
 
 }
